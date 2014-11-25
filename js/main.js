0e8192ba381dad8bf033fb2bb6cd68ec6b0e0756
corm0078 <corm0078@algonquinlive.com>
2014-11-25 11:36:36 -0500

    fixed nav to only appear at px height

--- js/main.js
+++ js/main.js
@@ -49,45 +49,12 @@ $('#menu a').on('click', function () {
   removeClass(theToggle, 'on');
 });
 
-/*
-this.$el.on( 'mouseenter.hoverdir, mouseleave.hoverdir', function( event ) {
-     
-    var $el = $( this ),
-        $hoverElem = $el.find( 'div' ),
-        direction = self._getDir( $el, { x : event.pageX, y : event.pageY } ),
-        styleCSS = self._getStyle( direction );
-     
-    if( event.type === 'mouseenter' ) {
-         
-        $hoverElem.hide().css( styleCSS.from );
-        clearTimeout( self.tmhover );
- 
-        self.tmhover = setTimeout( function() {
-             
-            $hoverElem.show( 0, function() {
-                 
-                var $el = $( this );
-                if( self.support ) {
-                    $el.css( 'transition', self.transitionProp );
-                }
-                self._applyAnimation( $el, styleCSS.to, self.options.speed );
- 
-            } );
-             
-         
-        }, self.options.hoverDelay );
-         
-    }
-    else {
-     
-        if( self.support ) {
-            $hoverElem.css( 'transition', self.transitionProp );
-        }
-        clearTimeout( self.tmhover );
-        self._applyAnimation( $hoverElem, styleCSS.from, self.options.speed );
-         
-    }
-         
-} );
 
-*/
+$(window).scroll(function(){
+      if($(window).scrollTop()>1000){
+         $(".background").fadeIn();
+      }else{
+         $(".background").fadeOut();
+      }
+
+});
